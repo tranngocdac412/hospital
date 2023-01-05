489c9d8b3c897c0cc2722ee7cd4aa2c5745e02f4
DAC Tran Ngoc (VTI.VMS) <dac.tranngoc@vti.vn>
2023-01-05 08:52:58 +0700

    set default value using context

--- wizards/create_appointment.py
+++ wizards/create_appointment.py
@@ -6,8 +6,8 @@ class CreateAppointment(models.TransientModel):
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     appointment_date = fields.Date(string='Appointment Date')
 
-    def create_appointment(self):
-        new_data = {}
-        new_data['patient_id'] = self.patient_id
-        new_data['appointment_date'] = self.appointment_date
-        self.env['hospital.appointment'].write(new_data)
+    # def create_appointment(self):
+    #     new_data = {}
+    #     new_data['patient_id'] = self.patient_id
+    #     new_data['appointment_date'] = self.appointment_date
+    #     self.env['hospital.appointment'].sudo().write(new_data)
