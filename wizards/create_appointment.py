ef88cb199cdcd0a66af239e656f5ccdd7334969e
DAC Tran Ngoc (VTI.VMS) <dac.tranngoc@vti.vn>
2023-01-09 14:09:32 +0700

    fix wizard - create button action

--- wizards/create_appointment.py
+++ wizards/create_appointment.py
@@ -6,8 +6,9 @@ class CreateAppointment(models.TransientModel):
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     appointment_date = fields.Date(string='Appointment Date')
 
-    # def create_appointment(self):
-    #     new_data = {}
-    #     new_data['patient_id'] = self.patient_id
-    #     new_data['appointment_date'] = self.appointment_date
-    #     self.env['hospital.appointment'].sudo().write(new_data)
+    def create_appointment(self):
+        pass
+        new_data = {}
+        new_data['patient_id'] = self.patient_id.id
+        new_data['appointment_date'] = self.appointment_date
+        self.env['hospital.appointment'].sudo().create(new_data)
